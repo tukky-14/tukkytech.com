e4a85736747751e64da971eb86cc5d250e9c057a
tukky-14 <bigoonage0104@gmail.com>
2023-04-29 22:08:52 +0900

    update：ナビゲーションバーに検索を追加

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                 </div>
             </nav>
             <nav className="bg-gray-50 dark:bg-gray-700">
-                <div className="max-w-screen-xl px-4 py-3 mx-auto">
+                <div className="max-w-screen-xl px-4 py-3 mx-auto flex justify-between">
                     <div className="flex items-center">
                         <ul className="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
                             <li>
@@ -55,6 +55,82 @@ const Navbar = () => {
                                 </a>
                             </li>
                         </ul>
+                    </div>
+                    <div className="flex md:order-2">
+                        <button
+                            type="button"
+                            data-collapse-toggle="navbar-search"
+                            aria-controls="navbar-search"
+                            aria-expanded="false"
+                            className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+                        >
+                            <svg
+                                className="w-5 h-5"
+                                aria-hidden="true"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                    clip-rule="evenodd"
+                                ></path>
+                            </svg>
+                            <span className="sr-only">Search</span>
+                        </button>
+                        <div className="relative hidden md:block">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg
+                                    className="w-5 h-5 text-gray-500"
+                                    aria-hidden="true"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                        clip-rule="evenodd"
+                                    ></path>
+                                </svg>
+                                <span className="sr-only">Search icon</span>
+                            </div>
+                            <input
+                                type="text"
+                                id="search-navbar"
+                                className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Search..."
+                            />
+                        </div>
+                    </div>
+                    <div
+                        className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                        id="navbar-search"
+                    >
+                        <div className="relative mt-3 md:hidden">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg
+                                    className="w-5 h-5 text-gray-500"
+                                    aria-hidden="true"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                        clip-rule="evenodd"
+                                    ></path>
+                                </svg>
+                            </div>
+                            <input
+                                type="text"
+                                id="search-navbar"
+                                className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Search..."
+                            />
+                        </div>
                     </div>
                 </div>
             </nav>
