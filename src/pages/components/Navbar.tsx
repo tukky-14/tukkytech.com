3920ccf6c16abd494327eec85503a0c9f1974366
tukky-14 <bigoonage0104@gmail.com>
2023-05-02 22:44:34 +0900

    update：Font Awesomeを使用できるようにnpmパッケージを追加

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import blog from '../../images/blog.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return (
@@ -46,14 +48,6 @@ const Navbar = () => {
                                     Team
                                 </a>
                             </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="text-gray-900 dark:text-white hover:underline"
-                                >
-                                    Features
-                                </a>
-                            </li>
                         </ul>
                     </div>
                     <div className="flex md:order-2">
@@ -64,37 +58,12 @@ const Navbar = () => {
                             aria-expanded="false"
                             className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
                         >
-                            <svg
-                                className="w-5 h-5"
-                                aria-hidden="true"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clip-rule="evenodd"
-                                ></path>
-                            </svg>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
                             <span className="sr-only">Search</span>
                         </button>
                         <div className="relative hidden md:block">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg
-                                    className="w-5 h-5 text-gray-500"
-                                    aria-hidden="true"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                        clip-rule="evenodd"
-                                    ></path>
-                                </svg>
-                                <span className="sr-only">Search icon</span>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
                             </div>
                             <input
                                 type="text"
@@ -110,19 +79,7 @@ const Navbar = () => {
                     >
                         <div className="relative mt-3 md:hidden">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg
-                                    className="w-5 h-5 text-gray-500"
-                                    aria-hidden="true"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                        clip-rule="evenodd"
-                                    ></path>
-                                </svg>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
                             </div>
                             <input
                                 type="text"
