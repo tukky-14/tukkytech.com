c78417eafb377dd82d4c692d582b21218af44ad2
tukky-14 <bigoonage0104@gmail.com>
2023-05-03 21:28:06 +0900

    fix：デプロイ時にfontawesomeが表示されない問題を修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
