a7f66a53b28a2d18b5d32ea92038d46f6d7d86a3
tukky-14 <bigoonage0104@gmail.com>
2023-05-06 22:45:04 +0900

    fix：サイト名を修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,15 +9,15 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <title>Jam Stack Blog</title>
+                <title>Tukky Tech Blog</title>
                 <meta name="description" content="Jam Stackで作成したBlogです。" />
-                <meta property="og:title" content="Jam Stack Blog" />
+                <meta property="og:title" content="Tukky Tech Blog" />
                 <meta property="og:description" content="Jam Stackで作成したBlogです。" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="" />
                 <meta property="og:image" content="" />
                 <meta property="og:locale" content="ja_JP" />
-                <meta property="og:site_name" content="Jam Stack Blog" />
+                <meta property="og:site_name" content="Tukky Tech Blog" />
             </Head>
             <Component {...pageProps} />
         </>
