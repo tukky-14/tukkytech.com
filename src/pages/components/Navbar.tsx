0f84a7573e35600087f72468fef5871a915fec59
tukky-14 <bigoonage0104@gmail.com>
2023-05-06 13:21:41 +0900

    update：モバイル画面時のナビゲーションバーを修正

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -27,21 +27,20 @@ const Navbar = (props: NavbarProps) => {
                     </button>
                 </div>
             </nav>
-            <nav className="hidden sm:block bg-gray-50 dark:bg-gray-700">
-                <div className="max-w-screen-xl px-4 py-3 mx-auto flex justify-between">
+            <nav className="bg-gray-50 dark:bg-gray-700">
+                <div className="max-w-screen-xl px-4 pt-3 mx-auto flex justify-between">
                     {isDetail ? (
                         <Link href="/" className="flex items-center text-gray-900 dark:text-white hover:underline">
                             <FontAwesomeIcon className="mr-2" icon={faArrowLeft} />
                             記事一覧に戻る
                         </Link>
                     ) : (
-                        <div className="flex items-center">
+                        <div className="flex items-center overflow-scroll">
                             <ul className="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
-                                <li>
+                                <li className="pb-3">
                                     <button
                                         onClick={handleAllClick}
-                                        className="text-gray-900 dark:text-white hover:underline"
-                                        aria-current="page"
+                                        className="w-12 text-gray-900 dark:text-white hover:underline"
                                     >
                                         すべて
                                     </button>
@@ -62,35 +61,8 @@ const Navbar = (props: NavbarProps) => {
                     )}
                     {!isDetail && (
                         <>
-                            <div className="flex md:order-2">
-                                <button
-                                    type="button"
-                                    data-collapse-toggle="navbar-search"
-                                    aria-controls="navbar-search"
-                                    aria-expanded="false"
-                                    className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
-                                >
-                                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                                    <span className="sr-only">Search</span>
-                                </button>
-                                <div className="relative hidden md:block">
-                                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                                    </div>
-                                    <input
-                                        type="text"
-                                        id="search-navbar"
-                                        className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        placeholder="Search..."
-                                        onChange={handleSearchChange}
-                                    />
-                                </div>
-                            </div>
-                            <div
-                                className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-                                id="navbar-search"
-                            >
-                                <div className="relative mt-3 md:hidden">
+                            <div className="flex sm:order-2">
+                                <div className="relative hidden sm:block">
                                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                         <FontAwesomeIcon icon={faMagnifyingGlass} />
                                     </div>
