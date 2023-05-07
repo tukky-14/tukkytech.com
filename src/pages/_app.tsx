fa24715f2ddecce637a8ab405732dc729a1e2c31
tukky-14 <bigoonage0104@gmail.com>
2023-05-07 21:21:57 +0900

    add：Google Analyticsのための処理を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,40 @@
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import * as gtag from '../libs/gtag';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import type { AppProps } from 'next/app';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouterChange = (url: any) => {
+            gtag.pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouterChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouterChange);
+        };
+    }, [router.events]);
+
+    return (
+        <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`} />
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.GA_TRACKING_ID}', {
+                        page_path: window.location.pathname,
+                    });
+                    `,
+                }}
+            />
+            <Component {...pageProps} />
+        </>
+    );
 }
