94c3a89ddfbb673bc1a61fd4557000542ea23e2e
tukky-14 <bigoonage0104@gmail.com>
2023-05-07 21:06:52 +0900

    fix：headタグの記述場所を修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,9 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Head>
-                <title>Tukky Tech Blog</title>
-                <meta name="description" content="Jam Stackで作成したBlogです。" />
-                <meta property="og:title" content="Tukky Tech Blog" />
-                <meta property="og:description" content="Jam Stackで作成したBlogです。" />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="" />
-                <meta property="og:image" content="" />
-                <meta property="og:locale" content="ja_JP" />
-                <meta property="og:site_name" content="Tukky Tech Blog" />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+    return <Component {...pageProps} />;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="ja">
+            <Head />
+            <title>Tukky Tech Blog</title>
+            <meta name="description" content="Jam Stackで作成したBlogです。" />
+            <meta property="og:title" content="Tukky Tech Blog" />
+            <meta property="og:description" content="Jam Stackで作成したBlogです。" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="" />
+            <meta property="og:image" content="" />
+            <meta property="og:locale" content="ja_JP" />
+            <meta property="og:site_name" content="Tukky Tech Blog" />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
