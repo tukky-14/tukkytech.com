05e89856ccd0d793feaf0120aaa8e7dff8222d40
tukky-14 <bigoonage0104@gmail.com>
2023-05-07 21:06:26 +0900

    fix：Footerを修正

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
                             target="_blank"
                             rel="noreferrer"
                         >
-                            tukky
+                            Tukky
                         </a>
                         . All Rights Reserved.
                     </span>
