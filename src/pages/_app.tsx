2660a16ab869539d91fa7a53ac815d9d04cdd456
tukky-14 <bigoonage0104@gmail.com>
2023-05-09 21:31:48 +0900

    fix：Google Analytics用の処理を修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,14 +21,14 @@ export default function App({ Component, pageProps }: AppProps) {
 
     return (
         <>
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`} />
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`} />
             <script
                 dangerouslySetInnerHTML={{
                     __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${process.env.GA_TRACKING_ID}', {
+                    gtag('config', '${gtag.GA_MEASUREMENT_ID}', {
                         page_path: window.location.pathname,
                     });
                     `,
