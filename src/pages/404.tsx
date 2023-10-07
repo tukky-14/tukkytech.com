ad13988ee2e8bd5edd5ba4cc4efe407736447eae
tukky-14 <bigoonage0104@gmail.com>
2023-10-07 14:49:48 +0900

    404ページにホームに戻る処理を追加

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function Custom404() {
     return (
         <main className="main">
             <p>ページがありません。</p>
+            <Link href="/" className="text-cyan-600 hover:underline">
+                ホームに戻る
+            </Link>
         </main>
     );
 }
