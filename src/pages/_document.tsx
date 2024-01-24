1af039b8becf384350676f2def09c2954d678fb7
tukky-14 <bigoonage0104@gmail.com>
2024-01-24 22:28:30 +0900

    モバイル端末からアプリを起動したとき、アドレスバーを非表示

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,8 @@ export default function Document() {
                 <meta property="og:site_name" content="Tukky Tech Blog" />
                 <meta property="og:locale" content="ja_JP" />
                 <meta property="og:description" content="Jam Stackで作成した技術ブログです。" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="mobile-web-app-capable" content="yes" />
                 <link rel="icon" href="/icon_16.png" />
                 <link rel="apple-touch-icon" sizes="192x192" href="/icon_192.png" />
             </Head>
