1a79864ee33bf14d85aff0404cda70fafa46a52d
tukky-14 <bigoonage0104@gmail.com>
2024-01-29 07:39:15 +0900

    IT用語の情報アイコンをクリックしたらモーダルを表示する処理を追加

--- src/pages/words2/index.tsx
+++ src/pages/words2/index.tsx
@@ -17,13 +17,15 @@ export type TestInfo = {
 export default function Words2({ words }: any) {
     const allWords = words;
     const [data, setData] = useState(() => [...allWords]);
+    const [showModal, setShowModal] = useState(false);
+    const [modalContent, setModalContent] = useState('');
 
     const columnHelper = createColumnHelper<TestInfo>();
 
     const columns = [
         columnHelper.accessor('id', {
             cell: (info) => (
-                <button className="">
+                <button className="" onClick={() => handleWordClick(info.row.original.description)}>
                     <FontAwesomeIcon className="text-blue-600" icon={faCircleInfo} />
                 </button>
             ),
@@ -52,6 +54,17 @@ export default function Words2({ words }: any) {
         data,
         getCoreRowModel: getCoreRowModel(),
     });
+
+    // wordをクリックしたときのハンドラ
+    const handleWordClick = (description: string) => {
+        setModalContent(description.replaceAll('\n', '<br>'));
+        setShowModal(true);
+    };
+
+    // モーダルを閉じるときのハンドラ
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
     return (
         <Container>
@@ -91,6 +104,21 @@ export default function Words2({ words }: any) {
                 </div>
             </div>
 
+            {showModal && (
+                <div
+                    className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+                    onClick={handleCloseModal}
+                >
+                    <div
+                        className="mx-4 w-full rounded-lg bg-white p-4 sm:w-3/4"
+                        onClick={(event) => event.stopPropagation()}
+                    >
+                        <div className="h-96 w-full overflow-scroll sm:flex sm:h-auto">
+                            <div className="px-4" dangerouslySetInnerHTML={{ __html: modalContent }} />
+                        </div>
+                    </div>
+                </div>
+            )}
             <Footer />
         </Container>
     );
